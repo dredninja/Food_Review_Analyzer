7b9cdec6d605ad7014fb294ff4ceadc87b706618
dredninja <165383034+dredninja@users.noreply.github.com>
2025-05-02 17:21:39 +0530

    Update

--- speech.py
+++ speech.py
@@ -1,11 +1,1 @@
-import azure.cognitiveservices.speech as speechsdk
 
-def from_mic():
-    speech_config = speechsdk.SpeechConfig(subscription="YourSpeechKey", region="YourSpeechRegion")
-    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
-
-    print("Speak into your microphone.")
-    result = speech_recognizer.recognize_once_async().get()
-    print(result.text)
-
-from_mic()
